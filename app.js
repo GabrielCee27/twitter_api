71bd1d4d728327853d907aba06b16b3b2c439b5f
GabrielCee27 <gabrielcee81@gmail.com>
2017-09-24 17:39:06 -0700

    get ids

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("Staring Application");
 
-var config = require('./config'); //keys
-//var config = require('./config_dev');
+//var config = require('./config'); //keys
+var config = require('./config_dev');
 
 var twitter = require('twitter');
 
@@ -11,3 +11,20 @@ var client = new twitter({
   access_token_key: config.access_token,
   access_token_secret: config.access_token_secret
 });
+
+//To get a list of ids of users you are following
+function get_ids() {
+  client.get('friends/ids', function(error, tweets, response) {
+    if (error) throw error;
+    else { //callback
+      console.log("Getting ids...");
+
+      var body = JSON.parse(response.body); //to make it workable
+      var ids = body.ids;
+
+      //console.log(ids);
+    }
+  });
+};
+
+//get_ids();
