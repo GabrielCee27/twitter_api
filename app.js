9d4c65024b7e022cac1741befcdb67a965557ddd
GabrielCee27 <gabrielcee81@gmail.com>
2017-10-22 15:14:26 -0700

    camelCaseFunctions

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var client = new twitter({
   access_token_secret: config.access_token_secret
 });
 
-var user_name = config.user_name;
+const user_name = config.user_name;
 
 var data_obj = {
   'friends': [], //users you follow
@@ -31,12 +31,12 @@ var data_obj = {
 };
 
 var fs = require('fs');
-var dir_data = './user_data/' + user_name + '_data.json';
+const dir_data = './user_data/' + user_name + '_data.json';
 
 console.log("Set up complete.");
 ///////////////////////////////////////////////////////////////////////////////
 
-function user_obj_constructor(name, bool = null) {
+function userObjConstructor(name, bool = null) {
   var obj = {
     'screen_name': name,
     'verified': bool
@@ -45,11 +45,11 @@ function user_obj_constructor(name, bool = null) {
 };
 
 //Returns JSON object from data file.
-function load_data() {
+function getObjFromDataFile() {
   return JSON.parse(fs.readFileSync(dir_data, 'utf8'));
 };
 
-function write_data(data_obj, message = 'Data file updated') {
+function writeToDataFile(data_obj, message = 'Data file updated') {
   fs.writeFile(dir_data, JSON.stringify(data_obj), function(err) {
     if (err)
       return console.log(err);
@@ -58,7 +58,7 @@ function write_data(data_obj, message = 'Data file updated') {
   });
 };
 
-function print_data(message = '') {
+function printDataFileToConsole(message = '') {
   console.log("Data file " + message + ":");
   console.log(fs.readFileSync(dir_data, 'utf8'));
 };
@@ -72,51 +72,61 @@ if (!fs.existsSync(dir_data)) {
       return console.log(err);
 
     console.log("New data file created and initiated.");
-    get_friends_ids(); //since the program is starting from scratch
+    getFriendsIds(); //since the program is starting from scratch
   });
 
 } else {
   console.log("Data file already exists.");
 
   //check if friends is populated
-  var temp_obj = load_data();
-  //print_data();
+  var temp_obj = getObjFromDataFile();
+  //printDataFileToConsole();
 
   if (temp_obj.friends.length > 0) {
     console.log("Data file, friends is populated.");
     data_obj.friends = temp_obj.friends;
-    console.log("data_obj.friends is updated");
+    console.log("data_obj.friends updated");
     //console.log("data_obj: " + JSON.stringify(data_obj));
+
     if (temp_obj.unfollowers.length > 0) {
+
       console.log("Data file, unfollowers is populated");
       data_obj.unfollowers = temp_obj.unfollowers;
-      console.log("data_obj.unfollowers is updated.");
-      get_users_lookup();
+      console.log("data_obj.unfollowers updated.");
+
+        if(temp_obj.unfollowers[0].verified != null){
+          console.log("Already checked if verified.");
+        }
+        else{
+          console.log("Not checked if verified.");
+          getUsersLookup();
+        }
+
     } else {
       console.log("Data files, unfollowers is not populated");
-      get_friendships();
+      getFriendships();
     }
 
   } else {
     console.log("Data file, friends is not populated.");
-    get_friends_ids();
+    getFriendsIds();
   }
 }
 
 //To get an array of friends of users you are following and updates data file
-function get_friends_ids() {
+function getFriendsIds() {
   console.log("Getting friends...");
   client.get('friends/ids', function(error, tweets, response) {
     if (error) throw error;
     else { //callback
 
       var body = JSON.parse(response.body); //to make it workable
-      update_friends(body.ids);
+      updateFriendsWith(body.ids);
     }
   });
 };
 
-function update_friends(array) {
+function updateFriendsWith(array) {
   data_obj.friends = array;
   console.log("data_obj.friends is updated with ids.");
 
@@ -125,13 +135,13 @@ function update_friends(array) {
       return console.log(err);
 
     console.log('Data file updated with new friend ids.');
-    get_friendships();
+    getFriendships();
   });
 };
 
 //Friends should be populated
-//get_friendships will populate database with names of users who don't follow back
-function get_friendships() {
+//getFriendships will populate database with names of users who don't follow back
+function getFriendships() {
   console.log("Getting friendships...");
 
   //console.log("length: " + data_obj.friends.length);
@@ -140,21 +150,21 @@ function get_friendships() {
   for (var i = 0; i < loops; i++) {
     console.log("i: " + i);
     var param = {
-      user_id: arr_of_friends(i).toString()
+      user_id: getArrayOfFriends(i).toString()
     };
 
     client.get('friendships/lookup', param, function(error, tweet, response) {
       if (error) {
         return console.log(error);
       } else {
-        check_connections(JSON.parse(response.body));
+        checkConnections(JSON.parse(response.body));
       }
     });
   }
 
   /* Testing
   var param = {
-    user_id: arr_of_friends(0).toString()
+    user_id: getArrayOfFriends(0).toString()
   };
 
   var param = {
@@ -166,7 +176,7 @@ function get_friendships() {
       return console.log(error);
     }
     else {
-      check_connections(JSON.parse(response.body));
+      checkConnections(JSON.parse(response.body));
     }
   });
   */
@@ -174,7 +184,7 @@ function get_friendships() {
 };
 
 //returns an array of 100 friend ids
-function arr_of_friends(start) {
+function getArrayOfFriends(start) {
   var arr = [];
   var length = data_obj.friends.length;
 
@@ -188,7 +198,7 @@ function arr_of_friends(start) {
 };
 
 //if user does not follow back, append to unfollowers
-function check_connections(body) {
+function checkConnections(body) {
   console.log("Checking connections...");
 
   var arr = [];
@@ -208,14 +218,14 @@ function check_connections(body) {
 
   };
   //console.log("arr: " + arr);
-  append_unfollowers(arr); //sends an array of screen names that don't follow back
+  appendUnfollowers(arr); //sends an array of screen names that don't follow back
 };
 
-function append_unfollowers(arr) {
+function appendUnfollowers(arr) {
   console.log("Appending unfollowers...");
 
   for (var i = 0; i < arr.length; i++) {
-    data_obj.unfollowers.push(user_obj_constructor(arr[i]));
+    data_obj.unfollowers.push(userObjConstructor(arr[i]));
   };
   //console.log("data_obj.unfollowers updated.");
   //console.log("data_obj: " + JSON.stringify(data_obj));
@@ -231,36 +241,36 @@ function append_unfollowers(arr) {
 
 };
 
-function get_users_lookup(i=0) {
+function getUsersLookup(i=0) {
   console.log("Looking up users...");
-  temp_obj = load_data();
+  temp_obj = getObjFromDataFile();
   console.log("data file unfollowers length: " + temp_obj.unfollowers.length);
 
   console.log("Loop #: " + i);
   var param = {
-    screen_name: arr_of_unfollowers(i).toString()
+    screen_name: getArrayOfUnfollowers(i).toString()
   };
 
   client.get('users/lookup', param, function(error, tweet, response) {
     if (error) {
       return console.log(error);
     } else {
-      check_if_verified(JSON.parse(response.body), i);
+      checkIfUserIsVerified(JSON.parse(response.body), i);
 
       if(i == (Math.floor(temp_obj.unfollowers.length / 100)) ){
         console.log("End of recursion.");
-        write_data(data_obj);
+        writeToDataFile(data_obj);
       }
       else {
-        get_users_lookup(i + 1);
+        getUsersLookup(i + 1);
       }
     }
   });
 };
 
-function arr_of_unfollowers(start) {
+function getArrayOfUnfollowers(start) {
   var arr = [];
-  var temp_obj = load_data();
+  var temp_obj = getObjFromDataFile();
   var length = temp_obj.unfollowers.length; //254
 
   for (var i = start * 100;
@@ -274,7 +284,7 @@ function arr_of_unfollowers(start) {
   return arr;
 };
 
-function check_if_verified(body, start) {
+function checkIfUserIsVerified(body, start) {
   console.log("Checking if verified...");
 
   var l = start * 100; //
@@ -285,9 +295,9 @@ function check_if_verified(body, start) {
   for (var j = 0; j < body.length; j++) {
     if (body[j].verified == true) {
       console.log(body[j].screen_name + " is verified.");
-      data_obj.unfollowers[l] = user_obj_constructor(body[j].screen_name, true);
+      data_obj.unfollowers[l] = userObjConstructor(body[j].screen_name, true);
     } else {
-      data_obj.unfollowers[l] = user_obj_constructor(body[j].screen_name, false);
+      data_obj.unfollowers[l] = userObjConstructor(body[j].screen_name, false);
     }
     l += 1;
   };
